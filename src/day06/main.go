e5c42fd4c65e8de55fad39428628fcf904b32eb1
David Siekut <davidsiekut@gmail.com>
2017-12-06 00:57:06 -0500

    day six part one

---
+++ src/day06/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	bank := [16]int{2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14}
+	mem := []string{"2885423155121513514"}
+	found := false
+	moves := 0
+
+	for !found {
+		max := 0
+		pos := 0
+		for p, e := range bank {
+			if e > max {
+				max = e
+				pos = p
+			}
+		}
+
+		distribute := bank[pos]
+		bank[pos] = 0
+		pos++
+		for distribute > 0 {
+			if pos == 16 {
+				pos = 0
+			}
+			bank[pos] = bank[pos] + 1
+			distribute--
+			pos++
+		}
+
+		str := ""
+		for _, a := range bank {
+			str += strconv.Itoa(a)
+		}
+
+		for _, a := range mem {
+			if a == str {
+				found = true
+			}
+		}
+
+		mem = append(mem, str)
+		moves++
+	}
+
+	fmt.Println(moves)
+}
